764396fc17c01b88d96fd5c9fafc83ff8140a28a
baberarjumand <baber.arjumand@gmail.com>
2020-04-15 12:59:47 +0300

    completed video 3.1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,22 +3,21 @@ import * as fb from "firebase/app";
 import "firebase/firestore";
 import { Course } from "../model/course";
 
-// my public db creds
-const config = {
-  apiKey: "AIzaSyBN-2nngePTAxS37o2BcmLDH4a-tGGybEg",
-  authDomain: "udemy-fireb-angularunicourse.firebaseapp.com",
-  databaseURL: "https://udemy-fireb-angularunicourse.firebaseio.com",
-  projectId: "udemy-fireb-angularunicourse",
-  storageBucket: "udemy-fireb-angularunicourse.appspot.com",
-  messagingSenderId: "852862296693",
-  appId: "1:852862296693:web:3a14fc2297486ac8e08e3c",
-  measurementId: "G-3MPGWWX4EF",
-};
-
-fb.initializeApp(config);
-const db = fb.firestore();
-// const settings = { timestampsInSnapshots: true };
-// db.settings(settings);
+// // my public db creds
+// const config = {
+//   apiKey: "AIzaSyBN-2nngePTAxS37o2BcmLDH4a-tGGybEg",
+//   authDomain: "udemy-fireb-angularunicourse.firebaseapp.com",
+//   databaseURL: "https://udemy-fireb-angularunicourse.firebaseio.com",
+//   projectId: "udemy-fireb-angularunicourse",
+//   storageBucket: "udemy-fireb-angularunicourse.appspot.com",
+//   messagingSenderId: "852862296693",
+//   appId: "1:852862296693:web:3a14fc2297486ac8e08e3c",
+//   measurementId: "G-3MPGWWX4EF",
+// };
+// fb.initializeApp(config);
+// const db = fb.firestore();
+// // const settings = { timestampsInSnapshots: true };
+// // db.settings(settings);
 
 @Component({
   selector: "about",
@@ -31,49 +30,50 @@ export class AboutComponent implements OnInit {
   ngOnInit() {
     /////////////////////////////////////////////////////////////////
     // start of video 2.3
-
+    //
     // db.doc("courses/wHs2CQZRPq9iMmA9T5Ja")
     //   .get()
     //   .then((snap) => console.log(snap.data()));
-
+    //
     // end of video 2.3
     /////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////
     // start of video 2.4
-
+    //
     // get courses collection with metadata
     // db.collection("courses")
     //   .get()
     //   .then((snaps) => console.log(snaps));
-
+    //
     // get array of courses
     // db.collection("courses")
     //   .get()
     //   .then((snaps) => {
     //     console.log(snaps.docs.map((snap) => snap.data()));
     //   });
-
+    //
     // get all course ids
     // db.collection("courses")
     //   .get()
     //   .then((snaps) => {
     //     console.log(snaps.docs.map((snap) => snap.id));
     //   });
-
+    //
     // get courses with ids
-    db.collection("courses")
-      .get()
-      .then((snaps) => {
-        const courses: Course[] = snaps.docs.map((snap) => {
-          return <Course>{
-            id: snap.id,
-            ...snap.data(),
-          };
-        });
-        console.log(courses);
-      });
-
+    // db.collection("courses")
+    //   .get()
+    //   .then((snaps) => {
+    //     const courses: Course[] = snaps.docs.map((snap) => {
+    //       return <Course>{
+    //         id: snap.id,
+    //         ...snap.data(),
+    //       };
+    //     });
+    //     console.log(courses);
+    //   });
+    //
     // end of video 2.4
     /////////////////////////////////////////////////////////////////
+
   }
 }

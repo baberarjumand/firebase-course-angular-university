2ab7f820696c9f5d6329868d9dfbb4c4ce9d71d7
baberarjumand <baber.arjumand@gmail.com>
2020-04-16 12:49:40 +0300

    completed video 3.5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Course } from "../model/course";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { AngularFirestore } from "@angular/fire/firestore";
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: "home",
@@ -14,26 +15,45 @@ export class HomeComponent implements OnInit {
   beginnerCourses$: Observable<Course[]>;
   advancedCourses$: Observable<Course[]>;
 
-  constructor(private fsdb: AngularFirestore) {}
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
     /////////////////////////////////////////////////////////////////
     // start of video 3.4
 
-    this.courses$ = this.fsdb
-      .collection("courses")
-      .snapshotChanges()
-      .pipe(
-        map((snaps) => {
-          // map all snapshots to a course array
-          return snaps.map((snap) => {
-            return <Course>{
-              id: snap.payload.doc.id,
-              ...(snap.payload.doc.data() as Course),
-            };
-          });
-        })
-      );
+    // this.courses$ = this.fsdb
+    //   .collection("courses")
+    //   .snapshotChanges()
+    //   .pipe(
+    //     map((snaps) => {
+    //       // map all snapshots to a course array
+    //       return snaps.map((snap) => {
+    //         return <Course>{
+    //           id: snap.payload.doc.id,
+    //           ...(snap.payload.doc.data() as Course),
+    //         };
+    //       });
+    //     })
+    //   );
+
+    // this.beginnerCourses$ = this.courses$.pipe(
+    //   map((courses) =>
+    //     courses.filter((course) => course.categories.includes("BEGINNER"))
+    //   )
+    // );
+
+    // this.advancedCourses$ = this.courses$.pipe(
+    //   map((courses) =>
+    //     courses.filter((course) => course.categories.includes("ADVANCED"))
+    //   )
+    // );
+
+    // end of video 3.4
+    /////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////
+    // start of video 3.5
+
+    this.courses$ = this.coursesService.loadAllCourses();
 
     this.beginnerCourses$ = this.courses$.pipe(
       map((courses) =>
@@ -47,7 +67,7 @@ export class HomeComponent implements OnInit {
       )
     );
 
-    // end of video 3.4
+    // end of video 3.5
     /////////////////////////////////////////////////////////////////
   }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+import { Course } from "../model/course";
+import { map, first } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CoursesService {
+  constructor(private fsdb: AngularFirestore) {}
 
-  constructor() { }
+  loadAllCourses(): Observable<Course[]> {
+    return this.fsdb
+      .collection("courses")
+      .snapshotChanges()
+      .pipe(
+        map((snaps) => {
+          // map all snapshots to a course array
+          return snaps.map((snap) => {
+            return <Course>{
+              id: snap.payload.doc.id,
+              ...(snap.payload.doc.data() as Course),
+            };
+          });
+        }),
+        first()
+      );
+  }
 }

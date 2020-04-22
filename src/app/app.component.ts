e709d403c9fad9e184ebc3c48771b076f8cf3cbb
baberarjumand <baber.arjumand@gmail.com>
2020-04-22 11:32:56 +0300

    completed video 4.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { map } from "rxjs/operators";
 export class AppComponent implements OnInit {
   isLoggedIn$: Observable<boolean>;
   isLoggedOut$: Observable<boolean>;
+  pictureUrl$: Observable<string>;
 
   constructor(private afAuth: AngularFireAuth) {}
 
@@ -18,6 +19,9 @@ export class AppComponent implements OnInit {
     this.afAuth.authState.subscribe((user) => console.log(user));
     this.isLoggedIn$ = this.afAuth.authState.pipe(map((user) => !!user));
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !loggedIn));
+    this.pictureUrl$ = this.afAuth.authState.pipe(
+      map((user) => (user ? user.photoURL : null))
+    );
   }
 
   logout() {

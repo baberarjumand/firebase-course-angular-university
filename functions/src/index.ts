061a27a647a42615de67fa27b2dd8c88a45ec547
baberarjumand <baber.arjumand@gmail.com>
2020-05-03 14:55:55 +0300

    completed video 8.3

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,8 +19,38 @@ import { fsdb } from "./init";
 ///////////////////////////////////////////////////////////////////////////////
 // start of video 8.2
 
+// // const express = require("express");
+// import * as express from "express";
+// const cors = require("cors");
+
+// const app = express();
+
+// app.use(
+//   cors({
+//     origin: true,
+//   })
+// );
+
+// app.get("/courses", async (request, response) => {
+//   const snaps = await fsdb.collection("courses").get();
+
+//   const courses: any = [];
+
+//   snaps.forEach((snap: any) => courses.push(snap.data()));
+
+//   response.status(200).json({ courses });
+// });
+
+// export const getCourses = functions.https.onRequest(app);
+
+// end of video 8.2
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// start of video 8.3
+
 // const express = require("express");
-import * as express from "express";
+import * as express from "express"; // this declaration is more type-safe
 const cors = require("cors");
 
 const app = express();
@@ -34,7 +64,7 @@ app.use(
 app.get("/courses", async (request, response) => {
   const snaps = await fsdb.collection("courses").get();
 
-  const courses: any = [];
+  const courses: any[] = [];
 
   snaps.forEach((snap: any) => courses.push(snap.data()));
 
@@ -43,5 +73,10 @@ app.get("/courses", async (request, response) => {
 
 export const getCourses = functions.https.onRequest(app);
 
-// end of video 8.2
+export const helloWorld = functions.https.onRequest((request, response) => {
+  //  response.send("Hello from Firebase!");
+  response.status(200).json({ message: "Hello World" });
+});
+
+// end of video 8.3
 ///////////////////////////////////////////////////////////////////////////////
--- functions/src/init.ts
+++ functions/src/init.ts
@@ -1,7 +1,14 @@
+const admin = require("firebase-admin");
 
-const admin = require('firebase-admin');
+// make sure you generate your own Firebase Admin SDK private key from:
+//  Firebase Console -> Project Settings -> Service Accounts -> Generate Private Key
+// const serviceAccount = require("./serviceAccountKey/serviceAccountKey.json");
+import * as data from "./serviceAccountKey/serviceAccountKey.json";
+const serviceAccount = (<any>data).serviceAccountKey;
 
- admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://udemy-fireb-angularunicourse.firebaseio.com",
+});
 
- export const fsdb = admin.firestore();
- 
+export const fsdb = admin.firestore();

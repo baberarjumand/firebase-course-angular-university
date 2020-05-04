118049c2651476a11250704aba835e33a23a678d
baberarjumand <baber.arjumand@gmail.com>
2020-05-04 20:06:12 +0300

    completed video 8.8

--- functions/src/image-upload.ts
+++ functions/src/image-upload.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 const path = require("path");
 const os = require("os");
 const mkdirp = require("mkdirp-promise");
+const spawn = require("child-process-promise").spawn;
 
 const { Storage } = require("@google-cloud/storage");
 const gCloudService = new Storage();
@@ -9,6 +10,12 @@ const gCloudService = new Storage();
 export const resizeThumbnail = functions.storage
   .object()
   .onFinalize(async (object, context) => {
+    ///////////////////////////////////////////////////////////////////////////////
+    // start of video 8.6
+
+    // onFinalize() executes when a file upload to firebase storage completes
+    // the 'object' in this function is the uploaded image
+
     const fileFullPath = object.name || "";
     const contentType = object.contentType || "";
     const fileDir = path.dirname(fileFullPath);
@@ -20,10 +27,21 @@ export const resizeThumbnail = functions.storage
     // console.log(fileDir);
     // console.log(fileName);
 
-    if (!contentType.startsWith("image/")) {
+    // this function will exit if the uploaded file type is not an image, or
+    //  the uploaded image name starts with thumb_, meaning
+    //  it is a generated thumbnail from this function itself
+    if (!contentType.startsWith("image/") || fileName.startsWith("thumb_")) {
       console.log("File contentType is not an image. Exitting now...");
       return null;
     }
+
+    // end of video 8.6
+    ///////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // start of video 8.7
+
+    // NEXT STEP: download uploaded image to temp dir on OS in container
 
     // create sub-directories in os temp folder
     await mkdirp(localTempDir);
@@ -37,6 +55,53 @@ export const resizeThumbnail = functions.storage
     console.log("Downloading image to: ", tempLocalFile);
 
     await originalImageFile.download({ destination: tempLocalFile });
+
+    // end of video 8.7
+    ///////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // start of video 8.8
+
+    // NEXT STEP: generate a thumbnail using imageMagick
+
+    // container on gCloud already has some pre-installed binaries, incl. imageMagick
+    //  so we don't have to install it using npm
+
+    const outputFilePath = path.join(fileDir, "thumb_" + fileName);
+
+    const outputFile = path.join(os.tmpdir(), outputFilePath);
+
+    console.log("Generating a thumbnail to: ", outputFile);
+
+    // example of cli command to use ImageMagick:
+    // convert -thumbnail 510x287 serverless-angular.png > thumb_serverless-angular.png
+
+    await spawn(
+      "convert",
+      [tempLocalFile, "-thumbnail", "510x287 >", outputFile],
+      { capture: ["stdout", "stderr"] }
+    );
+
+    // NEXT STEP: upload generated thumbnail back to storage
+
+    const metadata = {
+      contentType: object.contentType,
+      cacheControl: "public.max-age=2592000, s-maxage=2592000",
+    };
+
+    console.log(
+      "Uploading generated thumbnail to storage: ",
+      outputFile,
+      outputFilePath
+    );
+
+    await bucket.upload(outputFile, {
+      destination: outputFilePath,
+      metadata: metadata,
+    });
+
+    // end of video 8.8
+    ///////////////////////////////////////////////////////////////////////////////
 
     return null;
   });
